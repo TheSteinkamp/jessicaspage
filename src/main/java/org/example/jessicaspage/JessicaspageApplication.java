9f77f55f31b33af1d30817d1c0f0df5ece0b67d9
Jessica Steinkamp <jessica@steinkamp.se>
2025-09-21 14:03:27 +0200

    Initial commit

---
+++ src/main/java/org/example/jessicaspage/JessicaspageApplication.java
@@ -0,0 +1,13 @@
+package org.example.jessicaspage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JessicaspageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JessicaspageApplication.class, args);
+    }
+
+}
