2e0be124b887c37298ae960c7941d887cd89f7aa
Jessica Steinkamp <jessica@steinkamp.se>
2025-09-22 18:27:56 +0200

    talking to frontend and saving in database OK

--- src/main/java/org/example/jessicaspage/contactcontroller.java
+++ src/main/java/org/example/jessicaspage/contactcontroller.java
@@ -2,13 +2,11 @@ package org.example.jessicaspage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/jessica")
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class contactcontroller {
 
     private final contactrepository contactrepository;
